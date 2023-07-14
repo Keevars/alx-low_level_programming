8ac5aa5303925e35222d3c10faafbb39fadb0b19
Keevars <uviwemakiva@gmail.com>
2023-07-13 18:12:50 -0700

    local files

---
+++ 4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main entry point - a program that prints a line with puts function
+ * Return: 0 (success indicator)
+ */
+
+int main(void)
+{
+puts{"\"Programming is like building a multilingual puzzle"};
+return (0);
+}
